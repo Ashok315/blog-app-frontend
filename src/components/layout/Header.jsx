9586b3fa62eb2b0e164fe7ba62e547d74cc269c2
ashok.atce <ashok.atce@gmail.com>
2024-10-28 17:29:16 +0530

    improve perfomance and optimize pages

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -83,73 +83,69 @@ export const Header = () => {
   },[location])
 
   return ( 
-      <nav className='bg-white dark:bg-slate-800 py-[0.55rem] fixed top-0 w-full z-[60]'>
+      <nav className='bg-white dark:bg-slate-800 py-[0.55rem] fixed top-0 w-full z-[70]'>
         <ContentContainer className='flex gap-3 justify-between items-center'>
-          {/* logo */}
-          <div className='dark:brightness-[1.20] contrast-[0.85]'>
-            <Link to="/" ><img src={logo} className='w-[90px] h-auto max-w-[85px] md:max-w-[95px]' alt="logo" /></Link> 
-          </div>
+            {/* logo */}
+            <div className='dark:brightness-[1.20] contrast-[0.85]'>
+              <Link to="/" aria-label='Go to homepage'><img src={logo} className='w-[90px] h-auto max-w-[85px] md:max-w-[95px]' alt="logo" /></Link> 
+            </div>
 
-     
-          {/* searchBar */}
-          <div className='relative text-sm lg:ml-6'>
-              <form action="#"  onSubmit={handleSearch}>
-                  <Input placeholder="Search" name="search" value={searchText} onChange={handleChange} className="!h-[1.85rem] dark:bg-slate-700 dark:border-slate-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-400"  padding="!px-4 pr-[2.3rem]"></Input>
-                  <BsSearch className='absolute top-2.5 right-4 text-lightBorder dark:text-slate-400'></BsSearch>
-                </form>
-          </div>
-         
-          {/* navLinks */}
-          
-          <ul className='hidden md:flex md:gap-5 content-start lg:ml-6 text-[0.88rem] text-gray-700 dark:text-[#b6b8ba] font-semibold tracking-wide'>
-              <li><Link to="/" className={`border-b-2 ${location.pathname=='/'?'border-primary text-black dark:text-white':"border-transparent"} hover:text-black dark:hover:text-white ease-in-out duration-300 pb-1`}>Home</Link></li>
-              <li><Link to="/blogs" className={`border-b-2 ${location.pathname=='/blogs'?'border-primary text-black dark:text-white':"border-transparent"} hover:text-black dark:hover:text-white ease-in-out duration-300 pb-1`}>Blogs</Link></li>
-              <li><Link to="/about_us" className={`border-b-2 ${location.pathname=='/about_us'?'border-primary text-black dark:text-white':"border-transparent"} hover:text-black dark:hover:text-white ease-in-out duration-300 pb-1`}>About</Link></li>
-          </ul>
+            {/* searchBar */}  
+            <div className='relative text-sm lg:ml-6'>
+                <form action="#"  onSubmit={handleSearch}>
+                    <Input placeholder="Search" name="search" value={searchText} onChange={handleChange} className="!h-[1.85rem] dark:bg-slate-700 dark:border-slate-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-400"  padding="!px-4 pr-[2.3rem]"></Input>
+                    <BsSearch className='absolute top-2.5 right-4 text-lightBorder dark:text-slate-400'></BsSearch>
+                  </form>
+            </div>
+            
+            {/* navLinks */}     
+            <ul className='hidden md:flex md:gap-5 content-start lg:ml-6 text-[0.88rem] text-gray-700 dark:text-[#b6b8ba] font-semibold tracking-wide'>
+                <li><Link to="/" aria-label='home' className={`border-b-2 ${location.pathname=='/'?'border-primary text-black dark:text-white':"border-transparent"} hover:text-black dark:hover:text-white ease-in-out duration-300 pb-1`}>Home</Link></li>
+                <li><Link to="/blogs" aria-label='blogs' className={`border-b-2 ${location.pathname=='/blogs'?'border-primary text-black dark:text-white':"border-transparent"} hover:text-black dark:hover:text-white ease-in-out duration-300 pb-1`}>Blogs</Link></li>
+                <li><Link to="/about_us" aria-label='about' className={`border-b-2 ${location.pathname=='/about_us'?'border-primary text-black dark:text-white':"border-transparent"} hover:text-black dark:hover:text-white ease-in-out duration-300 pb-1`}>About</Link></li>
+            </ul>
 
-          {/* themeButton */}
-          <div onClick={()=>dispatch(themeToggle())} className='hidden md:block lg:ml-6 border border-gray-500 rounded-full p-[0.33rem] hover:bg-gray-200 dark:hover:bg-gray-700 duration-200 cursor-pointer'>
-             {themeMode==="light"?<HiOutlineMoon className='text-[1rem] text-gray-800'/>
-                                 :<BiSun className='text-[1rem] text-gray-800 dark:text-white'/>
-             }
-          </div>
+            {/* themeButton */}
+            <div onClick={()=>dispatch(themeToggle())} role='button' aria-label='theme-mode' className='hidden md:block lg:ml-6 border border-gray-500 rounded-full p-[0.33rem] hover:bg-gray-200 dark:hover:bg-gray-700 duration-200 cursor-pointer'>
+                {themeMode==="light"?<HiOutlineMoon className='text-[1rem] text-gray-800'/>
+                                    :<BiSun className='text-[1rem] text-gray-800 dark:text-white'/>
+                }
+            </div>
+        
+            {isAuthenticated?
+                /* profile/auth-section */
+                <div ref={profileMenuRef} onClick={toggleProfileTag} className='hidden relative md:flex items-center gap-2 rounded-md py-1 px-4 duration-200 cursor-pointer'>
+                    
+                    <img src={userData?.image} alt="profile-image" className='w-[30px] h-[30px] max-w-[35px] max-h-[35px] rounded-full' />   
+                    <div className='flex item-center gap-1'>
+                      <p className='text-[0.8rem] text-gray-600 dark:text-gray-300 font-semibold capitalize'>{userData?.firstName+" "+userData?.lastName}</p>
+                      <IoMdArrowDropdown className='text-lg text-gray-600 dark:text-gray-300'/>
+                    </div>  
 
-          
-          {isAuthenticated?
-             /* profile/auth-section */
-              <div ref={profileMenuRef} onClick={toggleProfileTag} className='hidden relative md:flex items-center gap-2 rounded-md py-1 px-4 duration-200 cursor-pointer'>
-                 
-                  <img src={userData?.image} alt="profile-image" className='w-[30px] h-[30px] max-w-[35px] max-h-[35px] rounded-full' />   
-                  <div className='flex item-center gap-1'>
-                    <p className='text-[0.8rem] text-gray-600 dark:text-gray-300 font-semibold capitalize'>{userData?.firstName+" "+userData?.lastName}</p>
-                    <IoMdArrowDropdown className='text-lg text-gray-600 dark:text-gray-300'/>
-                  </div>  
+                    <div className={`${!isProfileTabOpen?'hidden':''} absolute bg-white dark:bg-slate-800 w-full py-2 top-10 left-0 rounded-md border border-gray-300 z-10`}>
+        
+                        <ul className='text-sm text-justify'> 
+                            <li className='leading-6'><Link to="/profile" onClick={toggleMenu} aria-label='profile' className="block hover:text-black hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white duration-300 px-3 py-1"><PiUserCircleFill className='text-[0.9rem] inline mr-2 mb-[2px]'/>Profile</Link></li>
+                            <li className='leading-6'><Link to="/add_blog" onClick={toggleMenu} aria-label='add blog' className="block  hover:text-black hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white duraton-300 px-3 py-1"><RiAddCircleFill className='text-[0.9rem] inline mr-2 mb-[2px]'/>Add Blog</Link></li>
+                            <li className='leading-6'><Link to={`/blogs/author/${userData?._id}`} onClick={toggleMenu} aria-label='my blogs' className="block  hover:text-black hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white duraton-300 px-3 py-1"><FaRectangleList className='text-[0.9rem] inline mr-2 mb-[2px]'/>My Blogs</Link></li>
+                            <li className='leading-6'><Link onClick={handleSignOut} aria-label='logout' className="block hover:text-black hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white duration-300 px-3 py-1"><IoMdLogOut className='text-[0.9rem] inline mr-2 mb-[2px]' />Logout</Link></li>
+                        </ul>
+                    </div>
+                </div>
+                
+              :  
+                /* login button */
+                <div className='hidden md:block py-1'>
+                  <Button type='button' onClick={()=>navigate('/sign_in')} aria-label="login" className='text-white bg-lightPrimary hover:bg-primary dark:bg-primary dark:hover:bg-lightPrimary duration-300'>Sign In</Button>
+                </div>
+              }
+            
 
-                  <div className={`${!isProfileTabOpen?'hidden':''} absolute bg-white dark:bg-slate-800 w-full py-2 top-10 left-0 rounded-md border border-gray-300 z-10`}>
-      
-                      <ul className='text-sm text-justify'> 
-                          <li className='leading-6'><Link to="/profile" onClick={toggleMenu} className="block hover:text-black hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white duration-300 px-3 py-1"><PiUserCircleFill className='text-[0.9rem] inline mr-2 mb-[2px]'/>Profile</Link></li>
-                          <li className='leading-6'><Link to="/add_blog" onClick={toggleMenu} className="block  hover:text-black hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white duraton-300 px-3 py-1"><RiAddCircleFill className='text-[0.9rem] inline mr-2 mb-[2px]'/>Add Blog</Link></li>
-                          <li className='leading-6'><Link to={`/blogs/author/${userData?._id}`} onClick={toggleMenu} className="block  hover:text-black hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white duraton-300 px-3 py-1"><FaRectangleList className='text-[0.9rem] inline mr-2 mb-[2px]'/>My Blogs</Link></li>
-                          <li className='leading-6'><Link onClick={handleSignOut} className="block  hover:text-black hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-white duration-300 px-3 py-1"><IoMdLogOut className='text-[0.9rem] inline mr-2 mb-[2px]' />Logout</Link></li>
-                      </ul>
-                  </div>
-              </div>
-              
-            :  
-              /* login button */
-              <div className='hidden md:block py-1'>
-                <Button type='button' onClick={()=>navigate('/sign_in')} className='text-white bg-lightPrimary hover:bg-primary dark:bg-primary dark:hover:bg-lightPrimary duration-300'>Sign In</Button>
-              </div>
-            }
-          
-
-          {/* menu-bar button */}
-          <div className='md:hidden'>
-            <FaBars className='text-4xl mr-[2px] border border-gray-400 rounded px-[0.6rem] cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 duration-200' onClick={toggleMenu} />
-          </div>
+            {/* menu-bar button */}
+            <div className='md:hidden'>
+              <FaBars className='text-4xl mr-[2px] border border-gray-400 rounded px-[0.6rem] cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 duration-200' onClick={toggleMenu} />
+            </div>
         </ContentContainer>
-        {/* </div> */}
         
   
         {/* Mobile menu bar */}
@@ -158,7 +154,7 @@ export const Header = () => {
               <div className='flex justify-between items-center gap-3 mb-3'>    
                   {/* logo */}
                   <div className='dark:brightness-[1.20] contrast-[0.85]'>
-                    <Link to="/" onClick={toggleMenu}><img src={logo} className='w-[90px] h-auto max-w-[85px] md:max-w-[95px]' alt="logo" /></Link>
+                    <Link to="/" onClick={toggleMenu} aria-label='go to homepage'><img src={logo} className='w-[90px] h-auto max-w-[85px] md:max-w-[95px]' alt="logo" /></Link>
                   </div>
 
                   {/* searchBar */}
@@ -168,7 +164,7 @@ export const Header = () => {
                       <BsSearch className='absolute top-2.5 right-4 text-lightBorder dark:text-slate-400'></BsSearch>
                     </form>
                   </div>
-
+                  
                   {/* menu-close-button */}
                   <div className='md:hidden'>
                     <IoClose className='text-4xl mr-[2px] border border-gray-400 rounded px-[0.32rem] cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 duration-200' onClick={toggleMenu}/>
@@ -178,9 +174,9 @@ export const Header = () => {
               {/* navLinks */}
               <div className='pb-[0.8rem] mb-[1rem] border-b-2 border-gray-300'>
                   <ul>
-                    <li className='leading-7 mt-1'><Link to="/" onClick={toggleMenu} className="block border-l-2 border-transparent rounded hover:border-primary hover:text-black dark:text-white hover:bg-slate-200 dark:hover:bg-gray-600 duration-300 px-3 py-1">Home</Link></li>
-                    <li className='leading-7 mt-1'><Link to="/blogs" onClick={toggleMenu} className="block border-l-2 border-transparent rounded hover:border-primary hover:text-black dark:text-white hover:bg-slate-200 dark:hover:bg-gray-600 duration-300 px-3 py-1">All Blogs</Link></li>
-                    <li className='leading-7 mt-1'><Link to="/about_us" onClick={toggleMenu} className="block border-l-2 border-transparent rounded hover:border-primary hover:text-black dark:text-white hover:bg-slate-200 dark:hover:bg-gray-600 duration-300 px-3 py-1">About</Link></li>
+                    <li className='leading-7 mt-1'><Link to="/" onClick={toggleMenu} aria-label='home' className="block border-l-2 border-transparent rounded hover:border-primary hover:text-black dark:text-white hover:bg-slate-200 dark:hover:bg-gray-600 duration-300 px-3 py-1">Home</Link></li>
+                    <li className='leading-7 mt-1'><Link to="/blogs" onClick={toggleMenu} aria-label='blogs' className="block border-l-2 border-transparent rounded hover:border-primary hover:text-black dark:text-white hover:bg-slate-200 dark:hover:bg-gray-600 duration-300 px-3 py-1">All Blogs</Link></li>
+                    <li className='leading-7 mt-1'><Link to="/about_us" onClick={toggleMenu} aria-label='about' className="block border-l-2 border-transparent rounded hover:border-primary hover:text-black dark:text-white hover:bg-slate-200 dark:hover:bg-gray-600 duration-300 px-3 py-1">About</Link></li>
                  </ul>
               </div>
                          
@@ -215,12 +211,13 @@ export const Header = () => {
                           :
                           // login-button
                             <div>
-                              <Button type='button' onClick={()=>{navigate('/sign_in'),toggleMenu()}} className='text-white bg-lightPrimary hover:bg-primary duration-300 dark:bg-primary dark:hover:bg-lightPrimary'>Sign In</Button>
+                              <Button type='button' onClick={()=>{navigate('/sign_in'),toggleMenu()}} aria-label="login" className='text-white bg-lightPrimary hover:bg-primary duration-300 dark:bg-primary dark:hover:bg-lightPrimary'>Sign In</Button>
                             </div>
                         }
                   </div>
+
                   {/* themeButton */}
-                  <div onClick={()=>dispatch(themeToggle())} className='border border-gray-500 rounded-full mr-4 p-[0.4rem] hover:bg-gray-200 dark:hover:bg-gray-700 duration-300 cursor-pointer'>
+                  <div onClick={()=>dispatch(themeToggle())} role='button' aria-label="theme-mode" className='border border-gray-500 rounded-full mr-4 p-[0.4rem] hover:bg-gray-200 dark:hover:bg-gray-700 duration-300 cursor-pointer'>
                                   {themeMode==="light"?<HiOutlineMoon className='text-[1rem] text-gray-800'/>
                                                       :<BiSun className='text-[1rem] text-gray-800 dark:text-white'/>
                                   }
--- src/components/main/Carousel/Carousel.jsx
+++ src/components/main/Carousel/Carousel.jsx
@@ -40,10 +40,7 @@ export const Carousel = () => {
         <section id="Carousel" className="mt-[3.5rem] carousel">
             <div className="flex justify-between items-center relative text-white">
                 {/*Left arrow button  */}
-                <Button
-                    className="absolute top-1/2 left-0 z-10 -translate-y-1/2 rounded-sm py-2 !px-[0.45rem] ml-1 bg-gray-600 bg-opacity-50 opacity-90 hover:bg-opacity-70 duration-200"
-                    onClick={handlePrevSlider}
-                    >
+                <Button onClick={handlePrevSlider} aria-label="previous-slide" className="absolute top-1/2 left-0 z-10 -translate-y-1/2 rounded-sm py-2 !px-[0.45rem] ml-1 bg-gray-600 bg-opacity-50 opacity-90 hover:bg-opacity-70 duration-200">
                         <BiLeftArrow className="text-xs" fetchpriority="low"></BiLeftArrow>    
                 </Button>
 
@@ -65,36 +62,27 @@ export const Carousel = () => {
                 
                 {/* inner carousel text in medium screen */}
                 <div className='hidden md:block absolute left-[6.5rem] z-50'>
-                    <p className='tracking-wide font-semibold text-[1.2rem] w-[56%] leading-[1.63rem]'> Start Your Blogging Journey Today And reach a global audience</p>
-                    <p className='pt-[0.65rem] w-[45%] leading-5 text-sm'>Empower your voice, build your audience, And make an impact with every post</p>
-                    <Button type='button' onClick={()=>navigate('/add_blog')} className='mt-[0.9rem] bg-primary text-white hover:bg-lightPrimary duration-300 rounded-md'>Start Writing Now</Button>
+                    <p className='tracking-wide font-semibold text-[1.15rem] w-[56%] leading-[1.6rem]'> Start Your Blogging Journey Today And reach a global audience</p>
+                    <p className='pt-[0.65rem] w-[50%] leading-[1.2rem] text-sm'>Empower your voice, build your audience, And make an impact with every post</p>
+                    <Button type='button' onClick={()=>navigate('/add_blog')} aria-label="create blog" className='mt-[0.9rem] bg-primary text-white hover:bg-lightPrimary duration-300 rounded-md'>Start Writing Now</Button>
                 </div>
                 
                 {/*inner carousel text in small screen*/}
                 <div className='md:hidden carousel-text w-[76vw] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-50'>
                   <p className='text-center font-semibold tracking-wide leading-[1.5rem]'>Start Your Blogging Journey Today And reach a global audience</p>
                   <div className='text-center'>
-                    <Button type='button' onClick={()=>navigate('/add_blog')} className='mt-[0.8rem] bg-primary text-white hover:bg-lightPrimary duration-300 rounded-md'>Start Writing Now</Button>
+                    <Button type='button' onClick={()=>navigate('/add_blog')} aria-label="create blog" className='mt-[0.85rem] bg-primary text-white hover:bg-lightPrimary duration-300 rounded-md'>Start Writing Now</Button>
                   </div>
                 </div>
 
                 {/* Right arrow button  */}
-                <Button
-                    className="absolute top-1/2 right-0 z-20 -translate-y-1/2 rounded-sm py-2 !px-[0.45rem] mr-1 bg-gray-600 bg-opacity-50 opacity-90 hover:bg-opacity-70 duration-200"
-                    onClick={handlerNextSlider}
-                    >
+                <Button onClick={handlerNextSlider} aria-label="next-slide" className="absolute top-1/2 right-0 z-20 -translate-y-1/2 rounded-sm py-2 !px-[0.45rem] mr-1 bg-gray-600 bg-opacity-50 opacity-90 hover:bg-opacity-70 duration-200">
                           <BiRightArrow className="text-xs" fetchpriority="low"></BiRightArrow>                 
                 </Button>
 
-                <div className="absolute bottom-1 -translate-x-1/2 left-1/2 text-white bg-gray-600 bg-opacity-50 opacity-90 px-1 pt-1 rounded-md">
+                <div className="absolute bottom-1 -translate-x-1/2 left-1/2 text-white bg-gray-600 bg-opacity-50 opacity-90 px-1 pt-1 rounded-md z-[60]">
                 {imageData.map((image, index) => (
-                    <button
-                    key={image}
-                    onClick={() => {
-                        setImageIndex(index);
-                    }}
-                    className="mx-[2px] hover:scale-110 duration-300"
-                    >
+                    <button key={image} onClick={() =>setImageIndex(index)} aria-label="slider-button" className="mx-[2px] hover:scale-110 duration-300">
                     {imageIndex === index ? (
                             <PiCircleFill className="text-xs" fetchpriority="low"></PiCircleFill>
                     ) : (  
